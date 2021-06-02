33f00af0ed7fb647b361a97021699784edb80ced
BadmashonKaBadmash <mateena38@gmail.com>
2021-06-02 17:33:35 +0500

    project_list

--- resources/views/project_list.blade.php
+++ resources/views/project_list.blade.php
@@ -1,22 +1,22 @@
 @extends('layout.app')
 <style>
-   .div-img img{
-   width: 150px !important;
-   border-radius: 50% !important;
-   object-fit: contain !important;
+     table th{
+   /* border-top: none !important;
+   border-bottom: none !important; */
+   
+   text-align: center !important;
+   }
+   table td{
+   /* border-top: none !important; */
+   text-align: center !important;
    }
    .form-inline-custom{
    display: flex !important;
-   align-items: center !important;
+   align-items: flex-end !important;
    align-content: center;
    }
    .form-inline-custom label{
-   width: 85% !important;
-   margin-bottom: 0px !important;
-   margin-right: 5px
-   }
-   select{
-   color: #495057 !important
+   width: 25% !important;
    }
    .btn-grabar{
    background-color: #9B75A6 !important;
@@ -24,137 +24,90 @@
    padding: 10px 15px !important;
    font-size: 16px !important;
    }
-   hr{
-    border: 1px solid #727CD4 !important;
-   }
 </style>
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        <div class="card p-3">
-        <div class="row">
-               <div class="col-md-6">
-                  <div class="form-group form-inline-custom">
-                     <label style="width: 40% !important" for="exampleInputPassword1">Proyecto actual</label>
-                     <select class="js-example-basic-single w-100 select2-hidden-accessible" data-width="100%" data-select2-id="1" tabindex="-1" aria-hidden="true">
-                        <option value="TX" data-select2-id="3">Texas</option>
-                        <option value="NY" data-select2-id="12">New York</option>
-                        <option value="FL" data-select2-id="13">Florida</option>
-                        <option value="KN" data-select2-id="14">Kansas</option>
-                        <option value="HW" data-select2-id="15">Hawaii</option>
-                     </select>
-                  </div>
-               </div>
-               <div class="col-md-6">
-                  <div class="form-group form-inline-custom">
-                     <label style="width: 40% !important" for="exampleInputPassword1">Tarea actual</label>
-                     <select class="js-example-basic-single w-100 select2-hidden-accessible" data-width="100%" data-select2-id="1" tabindex="-1" aria-hidden="true">
-                        <option value="TX" data-select2-id="3">Texas</option>
-                        <option value="NY" data-select2-id="12">New York</option>
-                        <option value="FL" data-select2-id="13">Florida</option>
-                        <option value="KN" data-select2-id="14">Kansas</option>
-                        <option value="HW" data-select2-id="15">Hawaii</option>
-                     </select>
-                  </div>
-               </div>
-            </div>
-            <div class="row" style="    align-items: center;">
-               <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Avanca Tarea</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-               <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Requerimiento</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-               <div class="col-md-4 text-right">
-                   <h5 style="color: #727CD4">ASIGNADO</h5>
-               </div>
-               <div class="col-md-8">
-                  <hr>
-               </div>
-            </div>
-            <div class="row">
-               <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Candidad producida por el trabajdor</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-               <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Horas dedicados</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-            </div>
-            <div class="row">
-               <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Productividad</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-            </div>
-            <div class="row">
-               <div class="col-md-8">
-                  <hr>
-               </div>
-            </div>
-            <div class="row">
-               <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Unidades fallades</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-               <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Ratio unidades falladas</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-            </div>
-            <div class="row">
-               <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Unidades corregidas</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-               <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Unidades por corregir</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-            </div>
-            <div class="row">
-                <div class="col-md-8">
-                    <hr>
-                </div>
-            </div>
-            <div class="row">
-            <div class="col-md-4">
-                  <div class="form-group form-inline-custom">
-                     <label for="exampleInputPassword1">Requerimiento del dia</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password" aria-autocomplete="list">
-                  </div>
-               </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="d-flex" style="justify-content: space-between;   align-items: center;">
-                        <h4>Nobre del trabajador</h4>
-                        <button class="btn btn-grabar">GRABAR</button>
-                    </div>
-                </div>
-            </div>
+        <div class="card p-2">
+        @if(Session::has('info'))
+      <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('info') }}</p>
+      @endif
+      @if ($errors->any())
+      @foreach ($errors->all() as $error)
+      <p class="alert {{ Session::get('alert-class', 'alert-danger') }}">{{ $error }}</p>
+      @endforeach
+      @endif
+            
+
+<div class="row">
+    
+    <div class="col-md-12">
+    <div class="table-responsive">
+										<table class="table">
+											<thead>
+												<tr>
+													<th>Id</th>
+													<th>Project</th>
+                                                    <th>Location</th>
+                                                    <th>Contact Person</th>
+                                                    <th>Engineer Incharge</th>
+													<th>Amount</th>
+													<th>Start date</th>
+                                                    <th>End Date</th>
+                                                    <th>Status</th>
+                                                    <th></th>
+                                                    <th></th>
+
+												</tr>
+											</thead>
+											<tbody>
+                                            @foreach($projects as $project)
+												<tr>
+                                                <td></td>
+                                                <td>{{$project->project}}</td>
+													<td>{{$project->location}}</td>
+													<td>{{$project->contact_person}}</td>
+													<td>{{$project->engineer_incharge}}</td>
+                                                    <td>{{$project->amount}}</td>
+													<td>{{$project->start_date}}</td>
+													<td>{{$project->delivery_date}}</td>
+                                                    <td>@if($project->status == 0) 
+                                                                <option value="{{$project->status}}" data-select2-id="3">To start</option>  
+                                                                @endif  
+                                                                @if($project->status == 1) 
+                                                                <option value="{{$project->status}}" data-select2-id="3">On going</option>  
+                                                                @endif  
+                                                                @if($project->status == 2) 
+                                                                <option value="{{$project->status}}" data-select2-id="3">Stopped</option>  
+                                                                @endif  </td>
+                                                    
+                                                    
+                                                    <td>
+                                                    <form id="btn-submit"action="/change_status" method="post">
+                                                        <div class="form-group form-inline-custom">
+                                                        <input name="id" type="hidden" value="{{$project->id}}">
+									                        <select class="js-example-basic-single w-100 select2-hidden-accessible" value=""name="status" data-width="100%" data-select2-id="1" tabindex="-1" aria-hidden="true">
+                                                                
+                                                                <option value="2" @if($project->status == 2) selected @endif data-select2-id="12">stopped</option>
+                                                                <option value="1" @if($project->status == 1)selected @endif data-select2-id="13">On going</option>
+                                                                <option value="0"@if($project->status == 0)selected @endif data-select2-id="14">To start</option>    
+                                                            </select>
+							                            </div>
+                                                        <input type="submit" id="action_button" class="btn btn-primary btn-block" value="Change Status" />
+                                                        <input type="hidden" name="_token" value={{csrf_token()}}>
+                                                        </td>
+                                                       
+                                                    </form>
+                                                    
+                                            @endforeach	
+												</tr>
+											</tbody>
+										</table>
+								</div>
+
         </div>
     </div>
+</div>
+</div>
 </div>
 @endsection
