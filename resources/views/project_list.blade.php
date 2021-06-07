11a448bcb84861343ebdd62f75e17a4572be0080
daudsamim <l174017@lhr.nu.edu.pk>
2021-06-07 16:43:24 +0500

    daud changes

--- resources/views/project_list.blade.php
+++ resources/views/project_list.blade.php
@@ -23,14 +23,19 @@
    color: #fff !important;
    padding: 10px 15px !important;
    font-size: 16px !important;
+   }select.form-control, select, .email-compose-fields .select2-container--default select.select2-selection--multiple, .select2-container--default select.select2-selection--single, .select2-container--default .select2-selection--single select.select2-search__field, select.typeahead, select.tt-query, select.tt-hint {
+    color: black  !important;
    }
-</style>
+</style> 
 @section('content')
 <div class="row">
     <div class="col-md-12">
         <div class="card p-2">
         @if(Session::has('info'))
       <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('info') }}</p>
+      @endif
+      @if(Session::has('alert'))
+      <p class="alert {{ Session::get('alert-class', 'alert-danger') }}">{{ Session::get('alert') }}</p>
       @endif
       @if ($errors->any())
       @foreach ($errors->all() as $error)
@@ -40,66 +45,107 @@
             
 
 <div class="row">
-    
+   
     <div class="col-md-12">
+      <div class="div-flex" style="margin: 2%">
+          <h5>Status de los proyectos</h5>
+          
+      </div>
     <div class="table-responsive">
 										<table class="table">
 											<thead>
 												<tr>
-													<th>Id</th>
+												
 													<th>Project</th>
-                                                    <th>Location</th>
-                                                    <th>Contact Person</th>
-                                                    <th>Engineer Incharge</th>
-													<th>Amount</th>
-													<th>Start date</th>
-                                                    <th>End Date</th>
-                                                    <th>Status</th>
-                                                    <th></th>
-                                                    <th></th>
+                          <th>Start Date / Delivery Date</th>
+                          <th>Degree of Progress</th>
+                          <th>Task or Subtask in Progress</th>
+													<th>Cost Projection</th>
+													<th>Actual Cost</th>
+                          <th>Engineer in Charge</th>
+                          <th>Status</th>
+                          
 
 												</tr>
 											</thead>
 											<tbody>
-                                            @foreach($projects as $project)
+                        @foreach($projects as $project)
 												<tr>
-                                                <td>{{$project->id}}</td>
-                                                <td>{{$project->project}}</td>
-													<td>{{$project->location}}</td>
-													<td>{{$project->contact_person}}</td>
+                          <td>{{$project->project}}</td>
+													<td>{{$project->start_date}} --- {{$project->delivery_date}}</td>
+													<td>
+                                 <form id="btn-submit"action="/change_degree_of_progree" method="post">
+                                  <div class="form-group form-inline-custom">
+                                  <input name="id" type="hidden" value="{{$project->id}}">
+                                  <input type="text" style="text-align: center;" class="form-control" name="degree_of_progress" value="{{$project->degree_of_progress}}">
+                                  
+                                   </div>
+                                  
+                                  <input type="submit" id="action_button" class="btn btn-primary btn-block" value="Change Degree of Progress" />
+                                 
+                                  <input type="hidden" name="_token" value={{csrf_token()}}>
+                                  </td>
+                                 
+                              </form>             
+                          </td>
+													<td>
+                              <form id="btn-submit"action="/change_current_status" method="post">
+                                  <div class="form-group form-inline-custom">
+                                  <input name="id" type="hidden" value="{{$project->id}}">
+                    <select class="js-example-basic-single w-100 select2-hidden-accessible" value="" name="current_task" data-width="100%" data-select2-id="1" tabindex="-1" aria-hidden="true">
+                                           <option value="">Select Task</option>
+                                          @foreach($tasks as $row)
+                                          <option value="{{$row->task}}" @if($project->current_task == $row->task) selected @endif data-select2-id="12">{{$row->task}}</option>
+                                          @endforeach
+                                         
+                                      </select>
+                    </div>
+                                  <input type="submit" id="action_button" class="btn btn-primary btn-block" value="Change Task or Subtask" />
+                                  <input type="hidden" name="_token" value={{csrf_token()}}>
+                                  </td>
+                                 
+                              </form>
+                          </td>
+                          <td>${{$project->amount}}</td>
+													<td> 
+                              <form id="btn-submit"action="/change_actual_cost" method="post">
+                                  <div class="form-group form-inline-custom">
+                                  <input name="id" type="hidden" value="{{$project->id}}">
+                                  <input type="text" style="text-align: center;" class="form-control" name="actual_cost" value="{{$project->actual_cost}}">
+                                  
+                                   </div>
+                                  
+                                  <input type="submit" id="action_button" class="btn btn-primary btn-block" value="Change Actual Cost" />
+                                 
+                                  <input type="hidden" name="_token" value={{csrf_token()}}>
+                                  </td>
+                                 
+                              </form>
+                            </td>
 													<td>{{$project->engineer_incharge}}</td>
-                                                    <td>{{$project->amount}}</td>
-													<td>{{$project->start_date}}</td>
-													<td>{{$project->delivery_date}}</td>
-                                                    <td>@if($project->status == 0) 
-                                                                <option value="{{$project->status}}" data-select2-id="3">To start</option>  
-                                                                @endif  
-                                                                @if($project->status == 1) 
-                                                                <option value="{{$project->status}}" data-select2-id="3">On going</option>  
-                                                                @endif  
-                                                                @if($project->status == 2) 
-                                                                <option value="{{$project->status}}" data-select2-id="3">Stopped</option>  
-                                                                @endif  </td>
+                        
                                                     
                                                     
-                                                    <td>
-                                                    <form id="btn-submit"action="/change_status" method="post">
-                                                        <div class="form-group form-inline-custom">
-                                                        <input name="id" type="hidden" value="{{$project->id}}">
-									                        <select class="js-example-basic-single w-100 select2-hidden-accessible" value=""name="status" data-width="100%" data-select2-id="1" tabindex="-1" aria-hidden="true">
-                                                                
-                                                                <option value="2" @if($project->status == 2) selected @endif data-select2-id="12">stopped</option>
-                                                                <option value="1" @if($project->status == 1)selected @endif data-select2-id="13">On going</option>
-                                                                <option value="0"@if($project->status == 0)selected @endif data-select2-id="14">To start</option>    
-                                                            </select>
-							                            </div>
-                                                        <input type="submit" id="action_button" class="btn btn-primary btn-block" value="Change Status" />
-                                                        <input type="hidden" name="_token" value={{csrf_token()}}>
-                                                        </td>
-                                                       
-                                                    </form>
+                              <td>
+                              <form id="btn-submit"action="/change_status" method="post">
+                                  <div class="form-group form-inline-custom">
+                                  <input name="id" type="hidden" value="{{$project->id}}">
+                    <select class="js-example-basic-single w-100 select2-hidden-accessible" value=""name="status" data-width="100%" data-select2-id="1" tabindex="-1" aria-hidden="true">
+                                          <option value="0"@if($project->status == 0)selected @endif data-select2-id="14">Por iniciar</option>   
+                                          <option value="1" @if($project->status == 1)selected @endif data-select2-id="13">En marcha</option>
+                                          <option value="2" @if($project->status == 2) selected @endif data-select2-id="12">Detenido</option>
+                                          <option value="3" @if($project->status == 3) selected @endif data-select2-id="12">Parado</option>
+                                        
+                                           
+                                      </select>
+                    </div>
+                                  <input type="submit" id="action_button" class="btn btn-primary btn-block" value="Change Status" />
+                                  <input type="hidden" name="_token" value={{csrf_token()}}>
+                                  </td>
+                                 
+                              </form>
                                                     
-                                            @endforeach	
+                        @endforeach	
 												</tr>
 											</tbody>
 										</table>
